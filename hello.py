118fcbae908fd0bfcf8734d38ac4100f4244091d
brightlee14 <brightlee14@gmail.com>
2024-07-03 15:56:18 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
-# hello_world.py
+from flask import Flask
 
-def main():
-    print("Hello, World!")
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
 
 if __name__ == "__main__":
-    main()
-
+    app.run(host="0.0.0.0", port=5000)
